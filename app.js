17511ad415709cd6d151de1b7969a2ced25303ca
BalGrym <kevin_ber_69@hotmail.fr>
2024-10-07 14:48:48 +0200

    CORS package + origin CORS for LOCALHOST only |

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 require("dotenv").config();
 const port = process.env.PORT;
 
 //Middleware
 app.use(bodyParser.json());
+// CORS uniquement pour le localhost
+app.use(
+  cors({
+    origin: `${process.env.LOCALHOST}`,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 //Data-base Mongodb Mongoose
 const mongoose = require("mongoose");
